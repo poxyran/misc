40122b59b9ae150d47759071f91b0a8837298301
poxyran <poxyrantab@gmail.com>
2016-04-25 08:16:24 -0300

    Thanks to Arnaud Dideren for his help with function chunks.

--- make_dec_off.py
+++ make_dec_off.py
@@ -7,29 +7,37 @@ print "Enumerating functions..."
 
 ea = BeginEA()
 for funcea in Functions(SegStart(ea), SegEnd(ea)):
+        func = get_func(funcea)
+
 	functionName = GetFunctionName(funcea)
-
-	functionStart = GetFunctionAttr(funcea, FUNCATTR_START);
-	if functionStart == BADADDR:
-		QuitMsg(0, "Could not determine function start!");
-
-	functionEnd = GetFunctionAttr(funcea, FUNCATTR_END)
-	if functionEnd == BADADDR:
-		QuitMsg(0, "Could not determine function end!");    	
-
 	print "Function Name: %s" % functionName
-	print "Function Start: %x - Function End: %x" % (functionStart, functionEnd)
-	#print "Function chunks: %r" % Chunks(functionStart)
+	print "Function Start: %x - Function End: %x" % (func.startEA, func.endEA)
 
-	functionEnd -= 1
 	offset = 0
-	while functionStart+offset <= functionEnd:
-		MakeComm(functionStart+offset, "offset: %d" % offset)
-		print "Getting instruction at: %x" % (functionStart+offset)
-		insn = DecodeInstruction(functionStart+offset)
+	while func.startEA+offset <= func.endEA: # '<='? func.endEA is exclusive; IMO it should be '<'
+		MakeComm(func.startEA+offset, "%s+%d" % (functionName, offset))
+		#print "Getting instruction at: %x" % (functionStart+offset)
+		insn = DecodeInstruction(func.startEA+offset)
 		if insn != None:
-			print "Instruction size: %x" % insn.size
+			#print "Instruction size: %x" % insn.size
 			offset += insn.size
 		else:
-			print "Couldn't get instruction at %x" % (functionStart+offset)
+			print "Couldn't get instruction at %x" % (func.startEA+offset)
 			break
+
+	# handle function chunks
+	for chunk in Chunks(func.startEA):
+		if chunk[0] != func.startEA:
+		        print "Chunk start: %x - end: %x" %(chunk[0], chunk[1])
+			dist = abs(chunk[0] - func.startEA)
+			chunk_offset = 0
+			while chunk[0]+chunk_offset <= chunk[1]:
+				MakeComm(chunk[0]+chunk_offset, "(%x+%d)+%d" % (chunk[0], dist, chunk_offset))
+				insn = DecodeInstruction(chunk[0]+chunk_offset)
+				if insn != None:
+					chunk_offset += insn.size
+				else:
+					print "Couldn't get instruction at %x" %(chunk[0]+chunk_offset)
+					break
+
+Message("Done.\n")
